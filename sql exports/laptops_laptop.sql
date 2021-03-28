c8280cfeffa5557f818c313e0a7e385ab0f6f84a
Пользователь <tanasiuk.ahtiw@gmail.com>
2021-03-28 21:59:52 +0300

    add sql export file

---
+++ sql exports/laptops_laptop.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: laptops
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `laptop`
+--
+
+DROP TABLE IF EXISTS `laptop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `laptop` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `price` double NOT NULL,
+  `manufacturer` varchar(45) DEFAULT NULL,
+  `serial_number` varchar(45) DEFAULT NULL,
+  `processor` varchar(45) DEFAULT NULL,
+  `video_card` varchar(45) DEFAULT NULL,
+  `col_ram` double DEFAULT NULL,
+  `col_ram_vid` varchar(45) DEFAULT NULL,
+  `year_of_issue` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `laptop`
+--
+
+LOCK TABLES `laptop` WRITE;
+/*!40000 ALTER TABLE `laptop` DISABLE KEYS */;
+INSERT INTO `laptop` VALUES (1,'Hp Omen 17',17850,'Hewlett Packard','12557112','I5 8640H','Geforce Gtx1060',8,'2.0',2020),(2,'Asus Vivobook 5321sh',9888,'ASUS','12524341','I3 10140U','Nvidia m-350',8,'1.0',2020);
+/*!40000 ALTER TABLE `laptop` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-28 21:55:46
