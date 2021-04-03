a271ecedc166438e44419dfe88146c62b86fd855
Пользователь <tanasiuk.ahtiw@gmail.com>
2021-04-03 21:33:16 +0300

    add and cofigurate googlespreadsheet api

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 'use strict';
+const fs=require('fs');
+const readline=require('readline');
+const {google}=require('googleapis');
+const keys=require('./npm-googlespreadsheet-tut-36730a90b708.json');
 const express = require('express');
 const app=express();
 const mysql= require('mysql');
@@ -6,6 +10,20 @@ const bodyPasrser=require('body-parser');
 const port=process.env.port || 5000;
 app.use(bodyPasrser.urlencoded({extended:true}));
 app.use(bodyPasrser.json);
+const client=new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+);
+client.authorize(function(err,tokens){
+    if(err){
+        console.log(err);
+        return;
+    }else{
+        console.log('Connected');
+    }
+});
 const con=mysql.createConnection({
     host:'localhost',
     user:'root',
